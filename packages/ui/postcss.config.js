32670ece7974d9b93d2d02306975714a069b457e
chotaegyu <chotg5592@gmail.com>
2024-04-17 22:35:59 +0900

    FEAT: ui 에 tailwind 세팅

---
+++ packages/ui/postcss.config.js
@@ -0,0 +1,3 @@
+import postCssConfig from "@ddal-kkak/tailwind-config/postcss.config";
+
+export default postCssConfig;
---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,3 @@
+import tailwindConfig from "@ddal-kkak/tailwind-config/tailwind.config";
+
+export default tailwindConfig;
