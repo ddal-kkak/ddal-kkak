7a7d7d7b36fdb5905bf4a3a6527b364ac526286a
KodywiththeK <jmsds.km@gmail.com>
2024-05-01 00:30:46 +0900

    FEAT: 페이지 라우팅 설정

---
+++ apps/ddal-kkak/app/advanced/edit/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function AdvancedEditPage() {
+  return <div>page</div>
+}
---
+++ apps/ddal-kkak/app/block/edit/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function BlockEditPage() {
+  return <div>page</div>
+}
