6aa336e31d7f707f76d71e1acc5ceb6f3d170713
taegyu cho <49504411+0r0loo@users.noreply.github.com>
2024-05-04 17:50:56 +0900

    페이지 정보 기본 스키마 예시 코드 추가 (#45)
    * FEAT: 기본 스키마 추가
    
    * FEAT: 기본 스키마 폼 추가
    
    * Update apps/ddal-kkak/app/block/edit/_components/form.tsx
    
    index + 1 추가
    
    * FEAT: 바보짓함ㅋㅋㅋㅋ 빌드 오류 수정 파라미터에 +1 해버림

---
+++ apps/ddal-kkak/app/block/edit/_components/form.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { useFieldArray, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button, Input } from "@ddal-kkak/ui/atoms";
+import { PageSchema } from "../../../../schema";
+
+type PageSchemaType = z.infer<typeof PageSchema>;
+
+export default function TestForm() {
+  const method = useForm<PageSchemaType>({
+    resolver: zodResolver(PageSchema),
+  });
+  const { register, control } = method;
+
+  const { fields, append } = useFieldArray({
+    control,
+    name: "metaTagList",
+  });
+
+  const handleSubmit = (data: PageSchemaType) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Button
+        type={"button"}
+        onClick={() =>
+          append({
+            property: "",
+            content: "",
+          })
+        }
+      >
+        추가
+      </Button>
+      <form
+        {...method}
+        onSubmit={method.handleSubmit(handleSubmit)}
+        className={"flex flex-col"}
+      >
+        <label htmlFor="">
+          title
+          <Input type="text" {...register("info.title")} />
+        </label>
+        <label>
+          slug
+          <Input type="text" {...register("info.slug")} />
+        </label>
+        <label>
+          meta
+          <Input type="text" {...register(`metaTagList.0.property`)} />
+        </label>
+        <label htmlFor="">
+          content
+          <Input type="text" {...register(`metaTagList.0.content`)} />
+        </label>
+        {fields.map((field, index) => (
+          <>
+            <label>
+              meta
+              <Input
+                type="text"
+                key={field.id}
+                {...register(`metaTagList.${index + 1}.property`)}
+              />
+            </label>
+            <label htmlFor="">
+              content
+              <Input
+                type="text"
+                key={field.id}
+                {...register(`metaTagList.${index + 1}.content`)}
+              />
+            </label>
+          </>
+        ))}
+        <Button>제출</Button>
+      </form>
+    </>
+  );
+}
