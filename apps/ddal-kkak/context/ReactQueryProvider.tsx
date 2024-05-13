681b0b2380437c3c7a6eda1d6698202606598db8
KodywiththeK <jmsds.km@gmail.com>
2024-05-14 01:13:17 +0900

    FEAT: 리액트쿼리 init

---
+++ apps/ddal-kkak/context/ReactQueryProvider.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useState } from 'react'
+
+type Props = {
+  children: React.ReactNode
+}
+
+function ReactQueryProviders({ children }: Props) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+        },
+      },
+    })
+  )
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+export default ReactQueryProviders
