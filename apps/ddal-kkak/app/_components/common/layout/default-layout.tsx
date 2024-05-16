0db7242a0bac67b550e22758c800b678f0168fcb
chotaegyu <chotg5592@gmail.com>
2024-05-16 21:34:45 +0900

    FEAT: default layout 추가

---
+++ apps/ddal-kkak/app/_components/common/layout/default-layout.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from "react";
+import { cn } from "@ddal-kkak/shared";
+
+type DefaultLayoutProps = PropsWithChildren<{
+  className?: string;
+}>;
+
+export function DefaultLayout({ children, className }: DefaultLayoutProps) {
+  return (
+    <div className={cn("w-full max-w-4xl mx-auto sm:px-3", className)}>
+      {children}
+    </div>
+  );
+}
