9928347a44c522e1c14e97a113e28430e83811e3
chotaegyu <chotg5592@gmail.com>
2024-05-17 00:53:36 +0900

    FEAT: Spacer Block props zod 스키마로 변경

--- apps/ddal-kkak/app/_components/blocks/spacer/index.tsx
+++ apps/ddal-kkak/app/_components/blocks/spacer/index.tsx
@@ -1,9 +1,7 @@
-type SpacerProps = {
-  blockStyle?: {
-    height?: number;
-    backgroundColor?: string;
-  };
-};
+import { BlockSchemaProps } from "@/schemas/blocks";
+import { SpacerBlockSchema } from "@/schemas/blocks/spacer";
+
+type SpacerProps = BlockSchemaProps<typeof SpacerBlockSchema>;
 
 export function SpacerBlock({ blockStyle }: SpacerProps) {
   const { height = 1, backgroundColor = "transparent" } = blockStyle ?? {};
