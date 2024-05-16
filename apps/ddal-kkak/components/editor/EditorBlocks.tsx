26e51fea4624ae2879327333b4c0cbb0da0cf5ef
KodywiththeK <jmsds.km@gmail.com>
2024-05-16 22:50:18 +0900

    FEAT: 페이지 별 데이터 전달 (#60)

--- apps/ddal-kkak/components/editor/EditorBlocks.tsx
+++ apps/ddal-kkak/components/editor/EditorBlocks.tsx
@@ -1,6 +1,13 @@
+'use client'
 import TestForm from '@/app/edit/_components/form'
+import { PageType } from '@/types/pages'
 
-export default function EditorBlocks() {
+type EditorBlocksProps = {
+  page?: PageType
+}
+
+export default function EditorBlocks({ page }: EditorBlocksProps) {
+  console.log(page)
   return (
     <div className="w-full aspect-square border border-neutral-200 bg-white rounded-lg p-10">
       <TestForm />
--- apps/ddal-kkak/service/pages.ts
+++ apps/ddal-kkak/service/pages.ts
@@ -1,8 +1,17 @@
+import { PageType } from '@/types/pages'
 import customFetch, { CustomFetchConfig } from './fetch'
 
 export async function getPages() {
   const config: CustomFetchConfig = {
     endpoint: `pages`,
+    cache: 'no-store',
+  }
+  return customFetch(config)
+}
+export async function getPage(id: string | undefined): Promise<PageType | undefined> {
+  if (!id) return undefined
+  const config: CustomFetchConfig = {
+    endpoint: `pages/${id}`,
     cache: 'no-store',
   }
   return customFetch(config)
