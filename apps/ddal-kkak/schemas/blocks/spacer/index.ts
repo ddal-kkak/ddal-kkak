4b417efb6602796b03ca6c00d5004998531cc33e
chotaegyu <chotg5592@gmail.com>
2024-05-17 14:22:10 +0900

    FEAT: 유틸함수로 사용할 수 있도록 코드 변경

--- apps/ddal-kkak/schemas/blocks/spacer/index.ts
+++ apps/ddal-kkak/schemas/blocks/spacer/index.ts
@@ -1,23 +1,13 @@
 import { z } from "zod";
-import { transformStringToNumber } from "@ddal-kkak/shared/utils";
-
-const hexColorRegex = /^#([0-9A-F]{3}|[0-9A-F]{6})$/i;
+import { zodColor, zodStringNumber } from "@/schemas/blocks/util";
 
 export const SpacerBlockSchema = z.object({
   name: z.literal("SpacerBlock"),
   data: z.object({
     blockStyle: z
       .object({
-        height: z.preprocess(
-          (char) => transformStringToNumber(char as string),
-          z.number().optional(),
-        ),
-        backgroundColor: z
-          .string()
-          .optional()
-          .refine((val) => val === undefined || hexColorRegex.test(val), {
-            message: "유효한 HEX 컬러 문자열이 아닙니다.",
-          }),
+        height: zodStringNumber,
+        backgroundColor: zodColor,
       })
       .optional(),
   }),
