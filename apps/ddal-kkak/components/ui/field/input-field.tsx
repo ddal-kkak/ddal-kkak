c3e805a68a983223e164a623f163d7e4623511b3
chotaegyu <chotg5592@gmail.com>
2024-05-18 01:53:04 +0900

    FEAT: input 에러 수정

--- apps/ddal-kkak/components/ui/field/input-field.tsx
+++ apps/ddal-kkak/components/ui/field/input-field.tsx
@@ -8,10 +8,10 @@ type InputFieldProps = {
 };
 
 export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
-  function ({ label, isRequired }, ref) {
+  function ({ label, isRequired, ...rest }, ref) {
     return (
       <Label label={label} isRequired={isRequired}>
-        <Input ref={ref} size={"sm"} />
+        <Input ref={ref} size={"sm"} {...rest} />
       </Label>
     );
   },
